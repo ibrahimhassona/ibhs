9299281aa7eae683891536653340417db8233dce
ibrahim hassona <ibrahemhassona@gmail.com>
2024-10-19 20:41:58 +0300

    Update The Projects Loader

--- app/componentes/Loading.tsx
+++ app/componentes/Loading.tsx
@@ -1,9 +1,14 @@
+import { Loader2 } from 'lucide-react';
 const Loading = () => {
   return (
-    <div className=" font-semibold animate-pulse  duration-75 text-black font-roboto text-[16px] h-[calc(100vh-52px)] flex flex-col justify-center items-center">
-      Data Loading...
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <Loader2 className="h-20 w-20 animate-spin text-black mx-auto" />
+        <p className="mt-4 text-lg font-semibold text-gray-700">Projects Loading ...</p>
+      </div>
     </div>
   );
 };
 
 export default Loading;
+
