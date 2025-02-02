3c70681f294da09f5103b75757667edaa7f02dee
ibrahimhassona <ibrahemhasson@gmail.com>
2025-02-02 15:32:30 +0300

    Enhance OpenGraph Metadata

--- app/[locale]/projects/[project]/page.jsx
+++ app/[locale]/projects/[project]/page.jsx
@@ -1,19 +1,51 @@
 import Project from "@/app/componentes/Project";
 import { getProjects } from "@/lib/getProjects";
 
+// export async function generateMetadata({ params }) {
+//   const projects = await getProjects(`projects-${params.locale}`);
+//   const projectData = projects?.find(
+//     (project) => project.slug === params.project
+//   );
+
+//   return {
+//     title: projectData?.title,
+//     description: projectData
+//       ? `${projectData?.description}  ${
+//           projectData?.technologies ? projectData?.technologies.join(", ") : ""
+//         }`
+//       : "Frontend Project Details",
+//   };
+// }
 export async function generateMetadata({ params }) {
   const projects = await getProjects(`projects-${params.locale}`);
   const projectData = projects?.find(
     (project) => project.slug === params.project
   );
 
+  const title = projectData?.title || "Frontend Project";
+  const description = projectData
+    ? `${projectData.description} ${projectData.technologies ? projectData.technologies.join(", ") : ""
+    }`
+    : "Frontend Project Details";
+
+  const imageUrl = projectData?.image;
+
   return {
-    title: projectData?.title,
-    description: projectData
-      ? `${projectData?.description}  ${
-          projectData?.technologies ? projectData?.technologies.join(", ") : ""
-        }`
-      : "Frontend Project Details",
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: "website",
+      url: `https://hassouna.vercel.app/ar/projects/${params.project}`,
+      images: imageUrl,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: imageUrl,
+    },
   };
 }
 
